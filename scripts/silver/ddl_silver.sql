f3c259fb780c5838521a1c49a675a9ff62dc7c29
Felipe_ <144438404+FelipeAlvarezRodrigues@users.noreply.github.com>
2025-08-03 23:20:30 +0200

    Create ddl_silver.sql

---
+++ scripts/silver/ddl_silver.sql
@@ -0,0 +1,84 @@
+IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE silver.crm_cust_info;
+GO
+
+CREATE TABLE silver.crm_cust_info (
+	cst_id int,
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
+	cst_gndr NVARCHAR(50),
+	cst_create_date DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()  -- Metadata Columns/ DATETIME2 more precise than DATETIME
+);
+GO
+
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE silver.crm_prd_info;
+GO
+
+CREATE TABLE silver.crm_prd_info (
+	prd_id int,
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost DECIMAL (10,2),
+	prd_line NVARCHAR(50),
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()  -- Metadata Columns/ DATETIME2 more precise than DATETIME
+);
+GO
+
+IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE silver.crm_sales_details;
+GO
+
+CREATE TABLE silver.crm_sales_details (
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id int,
+	sls_order_dt NVARCHAR(50),
+	sls_ship_dt NVARCHAR(50),
+	sls_due_dt NVARCHAR(50),
+	sls_sales int,
+	sls_quantity int,
+	sls_price DECIMAL (10,2),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()  -- Metadata Columns/ DATETIME2 more precise than DATETIME
+);
+GO
+
+IF OBJECT_ID('silver.erp_CUST_AZ12', 'U') IS NOT NULL
+	DROP TABLE silver.erp_CUST_AZ12;
+GO
+
+CREATE TABLE silver.erp_CUST_AZ12 (
+	CID  NVARCHAR(50),
+	BDATE DATE,
+	GEN NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()  -- Metadata Columns/ DATETIME2 more precise than DATETIME	
+);
+GO
+
+IF OBJECT_ID('silver.erp_LOC_A101', 'U') IS NOT NULL
+	DROP TABLE silver.erp_LOC_A101;
+GO
+
+CREATE TABLE silver.erp_LOC_A101 (
+	CID NVARCHAR(50),
+	CNTRY NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()  -- Metadata Columns/ DATETIME2 more precise than DATETIME
+);
+GO
+
+IF OBJECT_ID('silver.erp_PX_CAT_G1V2', 'U') IS NOT NULL
+	DROP TABLE silver.erp_PX_CAT_G1V2;
+GO
+
+CREATE TABLE silver.erp_PX_CAT_G1V2 (
+	ID  NVARCHAR(50),
+	CAT  NVARCHAR(50),
+	SUBCAT  NVARCHAR(50),
+	MAINTENANCE  NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()  -- Metadata Columns/ DATETIME2 more precise than DATETIME
+);
